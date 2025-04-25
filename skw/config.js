c898bfe344b91b850c39e2b3093d95705b1795d5
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-04-26 02:03:19 +0700

    Update config.js

--- skw/config.js
+++ skw/config.js
@@ -1,41 +1,42 @@
-const { ethers } = require('ethers');
-const ora = require('ora');
+const amountLPUSDC = "10";
+const amountLPR2USD = "480";
+const amountswapUSDC = "980";
+const amountstakeR2USD = "490";
 
-const RPC_URL = 'https://base-sepolia-rpc.publicnode.com';
+const usdcAddress = "0xef84994eF411c4981328fFcE5Fda41cD3803faE4";
+const r2usdAddress = "0x20c54C5F742F123Abb49a982BFe0af47edb38756";
+const sr2usdAddress = "0xBD6b25c4132F09369C354beE0f7be777D7d434fa";
+const poolAddress = "0x07aBD582Df3D3472AA687A0489729f9F0424b1e3";
+const poolAddress2 = "0x61F2AB7B0C0E10E18a3ed1C3bC7958540374A8DC";
 
-const PRIOR_ADDRESS = '0xc19Ec2EEBB009b2422514C51F9118026f1cD89ba';
-const ROUTER_ADDRESS = '0x0f1DADEcc263eB79AE3e4db0d57c49a8b6178B0B';
-
-const ERC20_ABI = [
-  "function approve(address spender, uint256 amount) external returns (bool)",
-  "function balanceOf(address account) external view returns (uint256)",
-  "function allowance(address owner, address spender) external view returns (uint256)"
-];
-
-const SWAP_FUNCTION_SELECTORS = {
-  USDT: '0x03b530a3',
-  USDC: '0xf3b68002'
-};
+const swap_usdc = "0x095e7a95";
+const stake_r2u = "0x1a5f0f00";
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const amount = '0.01';
-const amountInWei = ethers.parseUnits(amount, 18);
+const addLP_abi = [
+  "function add_liquidity(uint256[] memory _amounts, uint256 _min_mint_amount, address _receiver) public returns (uint256)"
+];
 
-const provider = new ethers.JsonRpcProvider(RPC_URL);
-
-const spinner = ora({
-  color: "cyan",
-});
+const erc20_abi = [
+  "function approve(address spender, uint256 amount) external returns (bool)",
+  "function allowance(address owner, address spender) view returns (uint256)",
+  "function balanceOf(address account) external view returns (uint256)",
+];
 
 module.exports = {
-  PRIOR_ADDRESS,
-  ROUTER_ADDRESS,
-  ERC20_ABI,
-  SWAP_FUNCTION_SELECTORS,
+  amountLPUSDC,
+  amountLPR2USD,
+  amountswapUSDC,
+  amountstakeR2USD,
+  usdcAddress,
+  r2usdAddress,
+  sr2usdAddress,
+  poolAddress,
+  poolAddress2,
+  addLP_abi,
+  erc20_abi,
+  swap_usdc,
+  stake_r2u,
   delay,
-  amount,
-  amountInWei,
-  provider,
-  spinner
 };
